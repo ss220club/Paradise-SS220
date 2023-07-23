36ffcf8278ce63e439367984bdeafe000fda4cf3
Furior <furiorg@gmail.com>
2023-07-24 02:03:48 +0700

    "," -> ", "

--- tools/ci/unticked_files.py
+++ tools/ci/unticked_files.py
@@ -44,7 +44,7 @@ def get_unticked_files(root:Path):
             nested_dmes = [(includer_path + "/" if includer_path else "") + file for file in included if ".dme" in file]
             print(f'Found {len(included)} includes and {len(nested_dmes)} nested .dme\'s in {root / includer}')
             ticked_files.update([root / Path(includer).parent / Path(PureWindowsPath(i)) for i in included])
-            if nested_dmes: print(f"Additional include files: {','.join(nested_dmes)}")
+            if nested_dmes: print(f"Additional include files: {', '.join(nested_dmes)}")
             INCLUDER_FILES.extend([file.replace("\\", "/") for file in nested_dmes])
 
     all_dm_files = {f for f in root.glob('**/*.dm')}
