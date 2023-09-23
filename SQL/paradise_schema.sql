6daca6e452c240b4a90397ebef7c700398ec6af2
Furior <furiorg@gmail.com>
2023-09-23 20:45:40 +0700

    another one

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -635,8 +635,7 @@ CREATE TABLE `json_datum_saves` (
 -- Table structure for table `ckey_whitelist`
 --
 DROP TABLE IF EXISTS `ckey_whitelist`;
-CREATE TABLE `ckey_whitelist`
-(
+CREATE TABLE `ckey_whitelist` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`date` DATETIME DEFAULT now() NOT NULL,
 	`ckey` VARCHAR(32) NOT NULL,
@@ -652,12 +651,12 @@ CREATE TABLE `ckey_whitelist`
 -- Table structure for table `admin_wl`
 --
 CREATE TABLE `admin_wl` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `admin_rank` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Administrator',
-  `level` int(2) NOT NULL DEFAULT '0',
-  `flags` int(16) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `ckey` (`ckey`)
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`ckey` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`admin_rank` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Administrator',
+	`level` int(2) NOT NULL DEFAULT '0',
+	`flags` int(16) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`),
+	KEY `ckey` (`ckey`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
