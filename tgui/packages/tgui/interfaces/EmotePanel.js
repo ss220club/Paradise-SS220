fd9df16b1dac1d30a9a1cd2ac6d4ae8ac074ae6e
larentoun <larentoun@gmail.com>
2023-10-13 12:44:05 +0300

    it barely works, paracode moment

---
+++ tgui/packages/tgui/interfaces/EmotePanel.js
@@ -0,0 +1,94 @@
+import { useBackend, useSharedState } from '../backend';
+import { Window } from '../layouts';
+import { Button, Section, Input, LabeledList } from '../components';
+
+/*
+type Emote = {
+  key: string;
+  emote_path: string;
+  hands: BooleanLike;
+  visible: BooleanLike;
+  audible: BooleanLike;
+  sound: BooleanLike;
+};
+*/
+
+export const EmotePanelContent = (props, context) => {
+  const { act, data } = useBackend(context);
+  const { emotes } = data;
+
+  const [searchText, setSearchText] = useSharedState(
+    context,
+    'searchText',
+    ''
+  );
+
+  let searchBar = (
+    <Input
+      placeholder="Искать эмоцию..."
+      fluid
+      onInput={(e, value) => setSearchText(value)}
+    />
+  );
+
+  return (
+    <Section
+      title={
+        searchText.length > 0
+          ? `Результаты поиска "${searchText}"`
+          : `Все эмоции`
+      }
+      fill>
+      <LabeledList vertical fill>
+        <LabeledList.Item>
+          <Section>
+            {searchBar}
+          </Section>
+        </LabeledList.Item>
+        <LabeledList.Item>
+          {emotes.map((emote) =>
+            emote.key ? (
+              searchText.length > 0 ? (
+                emote.key.toLowerCase().includes(searchText.toLowerCase()) ? (
+                  <Button
+                    key={emote.key}
+                    onClick={() =>
+                      act('play_emote', {
+                        emote_path: emote.emote_path,
+                      })
+                    }>
+                    {emote.key.toUpperCase()}
+                  </Button>
+                ) : (
+                  ''
+                )
+              ) : (
+                <Button
+                  key={emote.key}
+                  onClick={() =>
+                    act('play_emote', {
+                      emote_path: emote.emote_path,
+                    })
+                  }>
+                  {emote.key.toUpperCase()}
+                </Button>
+              )
+            ) : (
+              ''
+            )
+          )}
+        </LabeledList.Item>
+      </LabeledList>
+    </Section>
+  );
+};
+
+export const EmotePanel = (props, context) => {
+  return (
+    <Window width={500} height={450}>
+      <Window.Content scrollable>
+        <EmotePanelContent />
+      </Window.Content>
+    </Window>
+  );
+};
