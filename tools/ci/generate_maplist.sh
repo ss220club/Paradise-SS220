9d1350fa8784c4818502ee933f52a24b8b576ae5
kyunkyunkyun <maxkrutoda0@gmail.com>
2025-04-23 12:19:33 +0500

    oops

--- tools/ci/generate_maplist.sh
+++ tools/ci/generate_maplist.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 # This script loops through every subdir here to generate a list of all maps programatically to avoid the list getting out of date
 
 cd _maps
-find map_files220 | grep -Ei ".dmm" | grep -v -e ".before" -e ".backup" > ci_map_testing.dm
+find | grep -Ei ".dmm" | grep -v -e ".before" -e ".backup" > ci_map_testing.dm
 sed -i -e 's/.*/#include \"&\"/' ci_map_testing.dm
 echo "Generated map compile file with `wc -l ci_map_testing.dm` maps"
