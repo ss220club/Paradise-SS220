80c97afd6056fdc94501b54db817157ea4e6fd3b
Mirka-l <anatolijx2000@gmail.com>
2026-05-10 23:24:33 +0300

    reverse migration to nanomap

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 # Generate maps
-tools/github-actions/dmm-tools-para minimap --enable minimaps -w 2040 -h 2040 "./_maps/map_files220/stations/boxstation.dmm"
-tools/github-actions/dmm-tools-para minimap --enable minimaps -w 2040 -h 2040 "./_maps/map_files220/stations/deltastation.dmm"
-tools/github-actions/dmm-tools-para minimap --enable minimaps -w 2040 -h 2040 "./_maps/map_files220/stations/metastation.dmm"
-# tools/github-actions/dmm-tools-para minimap --enable minimaps -w 2040 -h 2040 "./_maps/map_files/stations/cerestation.dmm"
-# tools/github-actions/dmm-tools-para minimap --enable minimaps -w 2040 -h 2040 "./_maps/map_files/stations/emeraldstation.dmm"
+tools/github-actions/dmm-tools-para minimap --enable nanomaps -w 2040 -h 2040 "./_maps/map_files220/stations/boxstation.dmm"
+tools/github-actions/dmm-tools-para minimap --enable nanomaps -w 2040 -h 2040 "./_maps/map_files220/stations/deltastation.dmm"
+tools/github-actions/dmm-tools-para minimap --enable nanomaps -w 2040 -h 2040 "./_maps/map_files220/stations/metastation.dmm"
+# tools/github-actions/dmm-tools-para minimap --enable nanomaps -w 2040 -h 2040 "./_maps/map_files/stations/cerestation.dmm"
+# tools/github-actions/dmm-tools-para minimap --enable nanomaps -w 2040 -h 2040 "./_maps/map_files/stations/emeraldstation.dmm"
 # Move and rename files so the game understands them
-mv "data/minimaps/boxstation-1.png" "icons/_minimaps/BoxStation220_minimap_z1.png"
-mv "data/minimaps/deltastation-1.png" "icons/_minimaps/DeltaStation220_minimap_z1.png"
-mv "data/minimaps/metastation-1.png" "icons/_minimaps/MetaStation220_minimap_z1.png"
-# mv "data/minimaps/cerestation-1.png" "icons/_minimaps/CereStation_minimap_z1.png"
-# mv "data/minimaps/emeraldstation-1.png" "icons/_minimaps/EmeraldStation_minimap_z1.png"
+mv "data/nanomaps/boxstation_nanomap_z1.png" "icons/_nanomaps/BoxStation220_nanomap_z1.png"
+mv "data/nanomaps/deltastation_nanomap_z1.png" "icons/_nanomaps/DeltaStation220_nanomap_z1.png"
+mv "data/nanomaps/metastation_nanomap_z1.png" "icons/_nanomaps/MetaStation220_nanomap_z1.png"
+# mv "data/nanomaps/cerestation_nanomap_z1.png" "icons/_nanomaps/CereStation_nanomap_z1.png"
+# mv "data/nanomaps/emeraldstation_nanomap_z1.png" "icons/_nanomaps/EmeraldStation_nanomap_z1.png"
